79762c774e7233ee676105c8a78d4cbea1f68cb7
Divya Wandhare <68555772+divyawandhare@users.noreply.github.com>
2022-07-21 18:37:58 +0530

    Create Cpp5.cpp

---
+++ Cpp5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int divisor, dividend, quotient, remainder;
+
+    cout << "Enter dividend: ";
+    cin >> dividend;
+
+    cout << "Enter divisor: ";
+    cin >> divisor;
+
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    cout << "Quotient = " << quotient << endl;
+    cout << "Remainder = " << remainder;
+
+    return 0;
+}
+
+OUTPUT :
+
+Enter dividend: 13
+Enter divisor: 4
+Quotient = 3
+Remainder = 1
